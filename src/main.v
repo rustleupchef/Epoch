0a3c1c938883c8d69cbf670006c5541d198f8f53
rustleupchef <rustle.up.chef@gmail.com>
2026-08-05 21:38:59 -0500

    added partial implementation of hacker news and news api recording

--- src/main.v
+++ src/main.v
@@ -1,19 +1,48 @@
 static func<void> main() {
-    string s = scrape("https://waiscshs-dev.onrender.com/");
-    pause(2000);
-    print(s);
+    string[] headlines = NewsAPI();
+    for (int i = 0; i < headlines.Length; i++) {
+        print(headlines[i]);
+    }
+    HackerNews();
 }
 
-static tooth<void> pause(int delay) language("csharp") => {
-    Thread.Sleep(delay);
-}
+static tooth<string[]> NewsAPI() language("javascript") => |
+    const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.key}`;
+    const res = syncRequest('GET', url, {
+        headers: {
+            'User-agent' : 'SnapshotApp/1.0'
+        }
+    });
 
-static tooth<string> scrape(string website) language("javascript") => {
+    if (res.statusCode === 200) {
 
-    const res = syncRequest('GET', website);
-    return res.body.toString('utf8');
-}
+        const data = JSON.parse(res.getBody('utf8'));
+        const headlines = [];
+        for (let article of data.articles) {
+            headlines.push(article.title);
+        }
 
-static tooth<void> print(string text) language("c") => {
-    printf(text);
-}
+        return headlines;
+    }
+
+    return [];
+|
+
+static tooth<void> HackerNews() language("javascript") => hacker_news
+    const topStoriesRes = syncRequest('GET', 'https://hacker-news.firebaseio.com/v0/topstories.json');
+    const storyIds = JSON.parse(topStoriesRes.getBody('utf8'))
+
+    console.log('--- Hacker News Top Stories ---');
+    
+    storyIds.forEach(id => {
+        const itemRes = syncRequest('GET', `https://hacker-news.firebaseio.com/v0/item/${id}.json`);
+        const item = JSON.parse(itemRes.getBody('utf8'));
+        console.log(`- [${item.score} points] ${item.title} (${item.url || 'No URL'})`);
+    });
+hacker_news
+
+static tooth<void> print(string text) language("C") => |
+    if (text != NULL) {
+        printf("%s\n", text);
+    }
+|
